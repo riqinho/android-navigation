508f9c60b4f97c4846ca30123a5f7f570d0882aa
Rick Alves Domingues <rickpoterdom@gmail.com>
2025-10-19 16:09:33 -0300

    passagem de parametros obrigatorios na tela de perfil

--- app/src/main/java/riqinho/com/github/androidnavigation/MainActivity.kt
+++ app/src/main/java/riqinho/com/github/androidnavigation/MainActivity.kt
@@ -42,8 +42,9 @@ class MainActivity : ComponentActivity() {
                         composable(route = "pedidos") {
                             PedidosScreen(modifier = Modifier.padding(innerPadding), navController)
                         }
-                        composable(route = "perfil") {
-                            PerfilScreen(modifier = Modifier.padding(innerPadding), navController)
+                        composable(route = "perfil/{nome}") {
+                            val nome: String? = it.arguments?.getString("nome", "Usuário Genárico")
+                            PerfilScreen(modifier = Modifier.padding(innerPadding), navController, nome!!)
                         }
                     }
                 }
